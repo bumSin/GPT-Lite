24ff388430cc8c2130f8a18e7e93130abb138f01
Shubham Singh <shubham.ksingh.cer14@itbhu.ac.in>
2024-09-14 21:17:20 +0530

    Minor refactoring, 'coz minor OCD :P

--- model/PositionalEmbedding.py
+++ model/PositionalEmbedding.py
@@ -4,14 +4,15 @@ import torch.nn as nn
 
 from config import ConfigManager
 
+
 class PositionalEmbedding(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.cfg = ConfigManager.get_config()
         self.positional_embedddings_map = nn.Parameter(torch.empty(self.cfg.n_ctx, self.cfg.d_model))
-        nn.init.normal_(self.positional_embedddings_map, std=self.cfg.init_range)  # To learn more about this, refer to my blog, link in README.md file
-
+        nn.init.normal_(self.positional_embedddings_map,
+                        std=self.cfg.init_range)  # To learn more about this, refer to my blog, link in README.md file
 
     def forward(self, x):
         '''
@@ -24,16 +25,16 @@ class PositionalEmbedding(nn.Module):
         seq_len = x.size(1)
 
         # Model learns pos embedding from pos 0 till pos: 1024 or n_ctx. Since seq_len might be less than 1024, we slice out pos embeddings for positions 0 to seq_len
-        pos_embeddings = self.positional_embedddings_map[:seq_len, :]   # pos_embeddings dim: seq_len, d_model
+        pos_embeddings = self.positional_embedddings_map[:seq_len, :]  # pos_embeddings dim: seq_len, d_model
 
         # Adding batch dim by Repeating across batch
-        pos_embeddings = einops.repeat(pos_embeddings, 'seq_len d_model -> batch seq_len d_model', batch = batch_size)  # batch seq_len d_model
+        pos_embeddings = einops.repeat(pos_embeddings, 'seq_len d_model -> batch seq_len d_model',
+                                       batch=batch_size)  # batch seq_len d_model
 
-        return pos_embeddings     # pos_embeddings dim: batch seq_len d_model
+        return pos_embeddings  # pos_embeddings dim: batch seq_len d_model
 
-
-x = torch.randn(32, 20)
-pe = PositionalEmbedding()
-
-em = pe(x)
-print(em.shape)   # expected shape: batch, seq_len, d_model
+# x = torch.randn(32, 20)
+# pe = PositionalEmbedding()
+#
+# em = pe(x)
+# print(em.shape)   # expected shape: batch, seq_len, d_model
