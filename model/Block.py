061b20333bc63aa64c20316e45d38bac1931cbc9
Shubham Singh <shubham.ksingh.cer14@itbhu.ac.in>
2024-09-16 13:32:30 +0530

    Adding one block

---
+++ model/Block.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+from model_layers.Feed_forward import Feed_forward
+from model_layers.LayerNorm import LayerNorm
+from model_layers.MultiHeadAttention import MultiHeadAttention
+
+
+class Block(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layer_norm1 = LayerNorm()
+        self.attn = MultiHeadAttention()
+
+        self.layer_norm2 = LayerNorm()
+        self.feedForward = Feed_forward()
+
+    def forward(self, resid_in):
+        '''
+        Input is read from residual stream with dim: (batch, seq_len, d_model)
+
+        :param x:
+        :return:
+        '''
+
+        norm_x = self.layer_norm1(resid_in)
+        attn_x = self.attn(norm_x)
+
+        resid_mid = resid_in + attn_x         # Writing to residual stream
+
+        norm_x = self.layer_norm2(resid_mid)
+        ff_x = self.feedForward(norm_x)
+
+        resid_out = resid_mid + ff_x
+
+        return resid_out
+
+blck = Block()
+x = torch.randn(200, 10, 768)
+x = blck(x)
+print(x.shape)
